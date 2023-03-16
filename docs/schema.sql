76002add843b2babe343ed629122a24c370bb361
Samuel Schmitz <sschmitz14@student.cvtc.edu>
2023-03-16 17:25:41 -0500

    'Characters Finished, Comics Not'

--- docs/schema.sql
+++ docs/schema.sql
@@ -1,20 +1,35 @@
 --Character Query
 DROP TABLE IF EXISTS character;
 CREATE TABLE character (
-    character_id INTEGER,
+    character_id SERIAL PRIMARY KEY,
     comic_id INTEGER,
-    name VARCHAR(50),
-    profile VARCHAR(50),
-    powers VARCHAR(50)
+    name VARCHAR(50) NOT NULL,
+    profile VARCHAR(50) NOT NULL,
+    powers VARCHAR(100) NOT NULL,
+    feedback INTEGER NOT NULL
+    --0 is N/A, 1 is best, 2 is worst
 );
 
+INSERT INTO character (name, profile, powers, feedback) VALUES 
+	('Black-Cat', 'White Female Anti-Hero', 'Super Strength, Super Agility, Grappling Hook', 0),
+	('Ultron', 'Masculine AI Villain', 'Hive Mind, Lasers, Magnets, Flight', 0), 
+	('Onslaught', 'Masculine Entity Villain', 'Telepathy, Telekinesis, Magnetism, Flight', 0),
+	('Psylocke', 'Asian Female Anti-Hero', 'Psychic Energy, Telepathy', 0),
+	('Spider-Man', 'White Male Hero', 'Spidey Sense, Superhuman Abilities, Web Shooters', 0),
+	('Ms-Marvel', 'Pakistani Female Hero', 'Shape Shift, Crystal Generation', 0),
+	('Moon-Knight', 'Jewish Male Hero', 'Regeneration, Superhuman Abilities, Cape Glider', 0),
+	('Venom', 'Masculine Symbiote Anti-Hero', 'Absorption, Regeneration, Enhance Abilities', 0),
+	('Hawkeye', 'White Male Hero', 'Mastered Archery, Martial Arts, Ronin Sword', 0),
+	('Iron-Man', 'White Male Hero', 'Armored Suit, Nano Tech, Lasers, Explosives, Flight, AI', 0),
+	('Star-Lord', 'White Male Hero', 'Celestial Powers, Quad Blasters, Explosives, Flight', 0),
+	('Shang-Chi', 'Asian Male Hero', '10 Rings, Martial Arts', 0);
 
 --Comic Query
 DROP TABLE IF EXISTS comic;
 CREATE TABLE comic (
-    comic_id INTEGER,
+    comic_id SERIAL PRIMARY KEY,
     character_id INTEGER,
-    author VARCHAR(50),
-    illustrator VARCHAR(50),
-    release_date VARCHAR(50)
+	title VARCHAR(50) NOT NULL,
+    creators VARCHAR(50) NOT NULL,
+    startYear VARCHAR(50) NOT NULL
 );
--- server.js
+++ server.js
@@ -66,23 +66,73 @@ express()
   .get('/about', function (req, res) {
     res.render('pages/about')
   })
-  .get('/character', function (req, res) {
-    res.render('pages/character')
-  })
-  .get('/comic', function (req, res) {
-    res.render('pages/comic')
-  })
   .get('/contact', function (req, res) {
     res.render('pages/contact')
   })
   .get('/character', async function (req, res) {
     const characterData = await queryAllCharacters()
-    console.log("characterData" + characterData)
+    console.log(characterData)
     res.render('pages/character', characterData)
   })
   .get('/comic', async function (req, res) {
     const comicData = await queryAllComics()
     res.render('pages/comic', comicData)
+  })
+  .post('/feedback', async function (req, res) {
+    res.set({ 'Content-Type': 'application.json' })
+
+    try {
+      const client = await pool.connect()
+
+      let feedback
+      if (req.body.feedback > 0 && req.body.feedback < 3) return 'Correct Feedback'
+      else {
+        console.error(`Unexpected feedback of ${req.body.feedback}`)
+        res.status(400).json({ ok: false })
+        return
+      }
+
+      const insertSql = `INSERT INTO feedback (feedback)
+        VALUES ($1::INTEGER, $2::FLOAT, NOW());`
+      await client.query(insertSql, [id, feedback])
+
+      res.json({ ok: true })
+      client.release()
+    } catch (err) {
+      console.err(err)
+      res.json({ buttons: [], error: err })
+    }
+  })
+
+  .get('/api', async function () {
+    const date = new Date().getTime()
+    const privKey = '5128770de98ee15fa624b7fe4c20412f12c43827'
+    const pubKey = '8db7b8ff6670008cd11d2540fe0cd63e'
+
+    const crypto = require('crypto')
+    const hash = crypto.createHash('md5').update(date + privKey + pubKey).digest("hex")
+    
+    const endpoint = ('"https://gateway.marvel.com:443/v1/public/comics?apikey=8db7b8ff6670008cd11d2540fe0cd63e&hash=" + hash + ""')
+
+    const response = await fetch(endpoint, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+
+    console.log(response)
+
+    if (response.ok) {
+      const result = await response.json()
+
+      console.log(result)
+    }
+
+    const client = await pool.connect()
+    const apiSQL = 'INSERT INTO comic (title, creators, startYear) VALUES (title ,creators, startYear);'
+    await client.query(apiSQL, [title, creators, startYear])
   })
   // end of implementation ///////////////////////////////////
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
